12bbc95e72148863f2766c6515caa02c969c72d8
Electricdragonfly25 <dougbowe25@gmail.com>
2025-08-07 13:04:10 +0100

    Add files via upload

---
+++ nested loop.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  4 23:17:57 2024
+
+@author: User
+"""
+for i in range(1,11):
+    for j in range(1,i):
+        print("#", end = "")
+    print()
+    
+print("-------------------")
+
+for i in range(10,1,-1):
+    for j in range(1,i):
+        print("#", end = "")
+    print()
+    
+print("-------------------")
+
+for i in range(1,6):
+    for j in range(1,6):
+        print("#", end = "")
+    print()
+    
+print("-------------------")
